a2f599fd855e89dd5b8b5c3baedddfd3314531e1
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-01-14 23:46:27 -0800

    changes in hero

--- src/components/HeroSlides.jsx
+++ src/components/HeroSlides.jsx
@@ -47,10 +47,10 @@ const HeroSlides = () => {
     };
   
     return (    
-      <div className='max-w-[1400px] h-[100vh] w-full m-auto py-16 px-16 relative group  heroBGC	'>
+      <div className='max-w-[1400px] h-[100vh] w-full m-auto py-16 px-4  relative group  heroBGC	'>
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})`, }}
-          className='w-full h-full  bg-center bg-cover duration-500'
+          className='w-full h-full  bg-center bg-cover duration-500 object-cover'
         ></div>
         {/* Left Arrow */}
         <div className=' absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
