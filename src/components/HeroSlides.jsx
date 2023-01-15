9fc12274b70ba6c367985965f1273697887cbba8
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-01-14 23:53:00 -0800

    fixing bug

--- src/components/HeroSlides.jsx
+++ src/components/HeroSlides.jsx
@@ -50,7 +50,7 @@ const HeroSlides = () => {
       <div className='max-w-[1400px] h-[100vh] w-full m-auto py-16 px-4  relative group  heroBGC	'>
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})`, }}
-          className='w-full h-full  bg-center bg-cover duration-500 object-cover'
+          className='w-full h-full object-contain bg-center bg-cover duration-500 object-cover'
         ></div>
         {/* Left Arrow */}
         <div className=' absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
